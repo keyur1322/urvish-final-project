dad226ae408b9425a954a0aec09eac3eeb2e63e0
urvish56 <panchalurvish56@gmail.com>
2024-04-09 10:32:29 +0530

    Urvish new edit

--- views/table/table.php
+++ views/table/table.php
@@ -43,6 +43,10 @@
                       <td><?php echo $row['customer_lname']; ?></td>
                       <td><?php echo $row['customer_mobilenumber']; ?></td>
                       <td><?php echo $row['customer_email']; ?></td>
+                      <td>
+                        <button class="btn btn-primary"><a href="update.php" class="text-light">Update</a></button>
+                        <button class="btn btn-danger"><a href="delete.php" class="text-light">Delete</a></button>
+                      </td>
                     </tr>
 
                   <?php
@@ -55,6 +59,9 @@
                   <?php
                 }
               ?>
+
+                
+
             </tbody>
           </table>
     </div>
